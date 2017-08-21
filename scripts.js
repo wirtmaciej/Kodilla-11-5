2c252e7fccd3a8c0b169aa67290b409633fc555f
Maciej Rut <wirtmaciej@gmail.com>
2017-08-21 09:10:32 +0200

    source files

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function Button(text) {
+	this.text = text || 'Hello!';
+}
+
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function() {
+			alert(self.text);
+		});
+		$('body').append(this.$element);
+	}
+}
+
+var btn1 = new Button('Hello!');
+btn1.create();
